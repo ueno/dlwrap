5b57f7ed63f9bda95ee7af6e47726ac021867c87
Daiki Ueno <dueno@redhat.com>
2024-05-02 13:41:06 +0900

    cargo fmt
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,14 @@ struct Cli {
     header: Vec<String>,
 }
 
-const LOADER_C_TEMPLATE: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/loader.c.template"));
-const LOADER_H_TEMPLATE: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/loader.h.template"));
+const LOADER_C_TEMPLATE: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/templates/loader.c.template"
+));
+const LOADER_H_TEMPLATE: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/templates/loader.h.template"
+));
 
 fn write_functions(
     input: impl AsRef<Path>,
@@ -68,13 +74,16 @@ fn write_functions(
         .write(true)
         .create(true)
         .open(output.as_ref())?;
-    
+
     let clang = Clang::new()?;
     let index = Index::new(&clang, false, false);
     let tu = index.parser(input.as_ref()).parse()?;
-    let funcs = tu.get_entity().get_children().into_iter().filter(|e| {
-        e.get_kind() == EntityKind::FunctionDecl
-    }).collect::<Vec<_>>();
+    let funcs = tu
+        .get_entity()
+        .get_children()
+        .into_iter()
+        .filter(|e| e.get_kind() == EntityKind::FunctionDecl)
+        .collect::<Vec<_>>();
     for func in funcs {
         let name = func.get_name().unwrap();
         if !patterns.iter().any(|pattern| pattern.is_match_at(&name, 0)) {
@@ -82,29 +91,40 @@ fn write_functions(
         }
 
         let result_type = func.get_result_type().unwrap();
-        let args = func.get_arguments().unwrap().into_iter().map(|arg| {
-            let type_ = arg.get_type().unwrap().get_display_name();
-            let delim = if type_.ends_with("*") { "" } else { " " };
-            format!("{}{}{}",
-                    type_,
-                    delim,
-                    arg.get_display_name().unwrap())
-        }).collect::<Vec<_>>();
-        let cargs = func.get_arguments().unwrap().into_iter().map(|arg| {
-            format!("{}",
-                    arg.get_display_name().unwrap())
-        }).collect::<Vec<_>>();
+        let args = func
+            .get_arguments()
+            .unwrap()
+            .into_iter()
+            .map(|arg| {
+                let type_ = arg.get_type().unwrap().get_display_name();
+                let delim = if type_.ends_with("*") { "" } else { " " };
+                format!("{}{}{}", type_, delim, arg.get_display_name().unwrap())
+            })
+            .collect::<Vec<_>>();
+        let cargs = func
+            .get_arguments()
+            .unwrap()
+            .into_iter()
+            .map(|arg| format!("{}", arg.get_display_name().unwrap()))
+            .collect::<Vec<_>>();
         let macro_ = if result_type.get_kind() == TypeKind::Void {
             "VOID_FUNC"
         } else {
             "FUNC"
         };
-        writeln!(&mut functions, "{}({}, {}, ({}), ({}))",
-                 macro_,
-                 result_type.get_display_name(),
-                 name,
-                 if args.len() == 0 { "void".to_string() } else { args.join(", ") },
-                 cargs.join(", "))?;
+        writeln!(
+            &mut functions,
+            "{}({}, {}, ({}), ({}))",
+            macro_,
+            result_type.get_display_name(),
+            name,
+            if args.len() == 0 {
+                "void".to_string()
+            } else {
+                args.join(", ")
+            },
+            cargs.join(", ")
+        )?;
     }
 
     Ok(())
@@ -130,30 +150,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let re = Regex::new("@(.*?)@")?;
-    let includes = cli.header.iter().map(|h| format!("#include <{}>", h)).collect::<Vec<_>>().join("\n");
+    let includes = cli
+        .header
+        .iter()
+        .map(|h| format!("#include <{}>", h))
+        .collect::<Vec<_>>()
+        .join("\n");
     let loader_h = format!("{}.h", &cli.loader);
     let functions_h = format!("{}funcs.h", &cli.loader);
-    let loader_uppercase = cli.loader.to_uppercase().replace(|c: char| {!(c.is_ascii_alphanumeric() || c == '_')}, "_");
+    let loader_uppercase = cli
+        .loader
+        .to_uppercase()
+        .replace(|c: char| !(c.is_ascii_alphanumeric() || c == '_'), "_");
     let guard = format!("{}_H_", &loader_uppercase);
     let enable_dlopen = format!("{}_ENABLE_DLOPEN", &cli.library_prefix.to_uppercase());
     let enable_pthread = format!("{}_ENABLE_PTHREAD", &cli.library_prefix.to_uppercase());
 
-    let replacement = |caps: &Captures| {
-        match &caps[1] {
-            "" => "@@",
-            "SYMBOL_PREFIX" => &cli.symbol_prefix,
-            "FUNCTION_PREFIX" => &cli.function_prefix,
-            "FUNCTIONS_H" => &functions_h,
-            "LIBRARY_SONAME" => &cli.soname,
-            "WRAPPER" => &cli.wrapper,
-            "ENABLE_DLOPEN" => &enable_dlopen,
-            "ENABLE_PTHREAD" => &enable_pthread,
-            "LIBRARY_PREFIX" => &cli.library_prefix,
-            "INCLUDES" => &includes,
-            "LOADER_H" => &loader_h,
-            "GUARD" => &guard,
-            _ => unreachable!(),
-        }
+    let replacement = |caps: &Captures| match &caps[1] {
+        "" => "@@",
+        "SYMBOL_PREFIX" => &cli.symbol_prefix,
+        "FUNCTION_PREFIX" => &cli.function_prefix,
+        "FUNCTIONS_H" => &functions_h,
+        "LIBRARY_SONAME" => &cli.soname,
+        "WRAPPER" => &cli.wrapper,
+        "ENABLE_DLOPEN" => &enable_dlopen,
+        "ENABLE_PTHREAD" => &enable_pthread,
+        "LIBRARY_PREFIX" => &cli.library_prefix,
+        "INCLUDES" => &includes,
+        "LOADER_H" => &loader_h,
+        "GUARD" => &guard,
+        _ => unreachable!(),
     };
 
     let mut loader_c = fs::OpenOptions::new()
@@ -172,11 +198,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let loader_h_content = re.replace_all(LOADER_H_TEMPLATE, replacement);
     loader_h.write_all(loader_h_content.into_owned().as_bytes())?;
 
-    write_functions(
-        &cli.input,
-        &cli.output.join(&functions_h),
-        &patterns,
-    )?;
+    write_functions(&cli.input, &cli.output.join(&functions_h), &patterns)?;
 
     Ok(())
 }
