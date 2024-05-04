033b2ac9b485e5abd5e19d3e837143be619d1550
Daiki Ueno <dueno@redhat.com>
2024-05-05 08:11:18 +0900

    ci: Cover scenario with run-time soname detection
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- examples/zstdver.c
+++ examples/zstdver.c
@@ -1,9 +1,18 @@
 #include "zstdwrap.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main (void)
 {
+#ifndef ZSTDWRAP_SONAME
+  const char *soname = getenv ("ZSTDWRAP_SONAME");
+  if (!soname)
+    abort();
+
+  zstdwrap_ensure_library (soname);
+#endif
+
   unsigned v1 = ZSTDWRAP_FUNC(ZSTD_versionNumber)();
   printf ("ZSTD_versionNumber: %u\n", v1);
   const char *v2 = ZSTDWRAP_FUNC(ZSTD_versionString)();
