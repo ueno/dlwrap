e2f404aafe1bc0ed0612216c7c14968e5460568c
Daiki Ueno <dueno@redhat.com>
2024-05-17 07:19:39 +0900

    tests: Update fixtures
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- fixtures/out/cgwrap.c
+++ fixtures/out/cgwrap.c
@@ -1,3 +1,10 @@
+/*
+ * Copying and distribution of this file, with or without modification,
+ * are permitted in any medium without royalty provided the copyright
+ * notice and this notice are preserved.  This file is offered as-is,
+ * without any warranty.
+ */
+
 #include "cgwrap.h"
 
 #if defined(CGWRAP_ENABLE_DLOPEN) && CGWRAP_ENABLE_DLOPEN
